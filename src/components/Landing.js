1976ce0192d9edc2304a612c310b3bb29f8ff77e
AmrShakhshirUDE <67582413+AmrShakhshirUDE@users.noreply.github.com>
2021-01-18 19:47:36 +0100

    Update Landing.js

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -7,12 +7,24 @@ import {Link, withRouter ,Switch} from 'react-router-dom';
 import Header from './/Header';
 
 
+import colorfulglobe from '../assets/images/colorfulglobe.png';
+import colorfulemail from '../assets/images/colorfulemail.png';
+import colorfulmembers from '../assets/images/MMembers.png';
+import colorfuldata from '../assets/images/colorfuldataset.png';
+import colorfulperformance from '../assets/images/colorfulperformance.png';
+import colorfulpiechart from '../assets/images/colorfulpiechart.png';
+
+import data from '../assets/images/data.png';
+// import barchart from '../assets/images/Barchart.png';
 import globe from '../assets/images/globe.png';
 import email from '../assets/images/email.png';
 import members from '../assets/images/members.png';
-import barchart from '../assets/images/Barchart.png';
-import data from '../assets/images/data.png';
 import performance from '../assets/images/performance.png';
+import piechart from '../assets/images/piechart.png';
+
+
+
+
 
 
 
@@ -36,7 +48,7 @@ export default class Landing extends Component {
                         <Col md="3">
                             <Link to="/Dataset">
                                 <Card body inverse id="DatasetBox" style={{ height:'200px',textAlign:'center',backgroundColor:'white', paddingTop: '40px' }}>
-                                    <CardText><img src={data} alt="phone" style={{'border': 'none','height': '60px','background-color':'#B06660'}}/></CardText>
+                                    <CardText><img src={colorfuldata} alt="Dataset" style={{'border': 'none','height': '60px'}} onMouseOver={e => (e.currentTarget.src =data )} onMouseOut={e => (e.currentTarget.src =colorfuldata )}/></CardText>
                                     <CardTitle tag="h6" style={{"color":"#B06660"}}>Dataset</CardTitle>
                                     {/* <Button>Button</Button> */}
                                 </Card>
@@ -45,7 +57,7 @@ export default class Landing extends Component {
                         <Col md="3">
                             <Link to="/Visualization">
                                 <Card body inverse  id="VisualizationBox" style={{ height:'200px',backgroundColor:'white' ,textAlign:'center', paddingTop: '40px'}}>
-                                    <CardText><img src={barchart} alt="phone" style={{'border': 'none','height': '60px','background-color':'#6A7D8E'}}/></CardText>
+                                    <CardText><img src={colorfulpiechart} alt="Visualization" style={{'border': 'none','height': '60px'}} onMouseOver={e => (e.currentTarget.src =piechart )} onMouseOut={e => (e.currentTarget.src =colorfulpiechart )}/></CardText>
                                     <CardTitle tag="h6" style={{"color":"#6A7D8E"}}>Visualization</CardTitle>
                                     {/* <Button color="secondary">Button</Button> */}
                                 </Card>
@@ -54,7 +66,7 @@ export default class Landing extends Component {
                         <Col md="3">
                             <Link to="/Performance">
                                 <Card body inverse  id="PerformanceBox" style={{ height:'200px',backgroundColor:'white', textAlign:'center', paddingTop: '40px'}}>
-                                    <CardText><img src={performance} alt="phone" style={{'border': 'none','height': '60px','background-color':'#AB9C73'}}/></CardText>
+                                    <CardText><img src={colorfulperformance} alt="Performance" style={{'border': 'none','height': '60px'}} onMouseOver={e => (e.currentTarget.src =performance )} onMouseOut={e => (e.currentTarget.src =colorfulperformance )}/></CardText>
                                     <CardTitle tag="h6" style={{"color":"#AB9C73"}}>Performance</CardTitle>
 
                                     {/* <Button color="secondary">Button</Button> */}
@@ -69,7 +81,7 @@ export default class Landing extends Component {
                         <Col md="3">
                             <a href="https://www.uni-due.de/soco/" target="_blank">
                                 <Card body inverse id="socoBox" style={{ height:'200px',textAlign:'center',backgroundColor:'white', paddingTop: '40px'}}>
-                                    <CardText><img src={globe} alt="phone" style={{'border': 'none','height': '60px','background-color':'#587992'}}/></CardText>
+                                    <CardText><img src={colorfulglobe} alt="socoBox" style={{'border': 'none','height': '60px' }} onMouseOver={e => (e.currentTarget.src =globe )} onMouseOut={e => (e.currentTarget.src =colorfulglobe )}/></CardText>
                                     <CardTitle tag="h6" style={{"color":"#587992"}}>Social Computing Group</CardTitle>
                                     {/* <Button color="secondary">Button</Button> */}
                                 </Card>
@@ -78,7 +90,7 @@ export default class Landing extends Component {
                         <Col md="3">
                             <Link to="/AboutUs">
                                 <Card body inverse  id="AboutUsBox"  style={{ height:'200px',textAlign:'center',backgroundColor:'white' , paddingTop: '40px'}}>
-                                <CardText><img src={members} alt="phone" style={{'border': 'none','height': '60px','background-color':'#74A181'}}/></CardText>
+                                <CardText><img src={colorfulmembers} alt="AboutUsBox" style={{'border': 'none','height': '60px'}} onMouseOver={e => (e.currentTarget.src =members )} onMouseOut={e => (e.currentTarget.src =colorfulmembers )}/></CardText>
                                     <CardTitle tag="h6" style={{"color":"#74A18E"}}>About us</CardTitle>
 
                                     {/* <Button color="secondary">Button</Button> */}
@@ -91,7 +103,7 @@ export default class Landing extends Component {
                         <Col md="3">
                             <Link to="/ContactUs">
                                 <Card body inverse  id="ContactUsBox"  style={{ height:'200px',textAlign:'center',backgroundColor:'white', paddingTop: '40px'}}>
-                                <CardText><img src={email} alt="phone" style={{'border': 'none','height': '60px','background-color':'#CA8F42'}}/></CardText>
+                                <CardText><img src={colorfulemail} alt="ContactUs" style={{'border': 'none','height': '60px'}} onMouseOver={e => (e.currentTarget.src =email )} onMouseOut={e => (e.currentTarget.src =colorfulemail )}/></CardText>
                                     <CardTitle tag="h6" style={{"color":"#CA8F42"}}>Contact us</CardTitle>
 
                                     {/* <Button color="secondary">Button</Button> */}
