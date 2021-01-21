f90fdc68847e1aca652e285894e642a4e4c3a5c6
AmrShakhshirUDE <67582413+AmrShakhshirUDE@users.noreply.github.com>
2021-01-21 18:34:48 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,108 +1,96 @@
-import React,{ Component } from 'react';
+import React, { Component } from 'react'
+import Footer from './Footer'
+import { Row,Card, CardTitle, CardText,Col } from 'reactstrap';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
-import { Row,Col,Card,Container,Nav,NavLink} from 'reactstrap';
-
+import Header from './/Header';
 import {Link, withRouter ,Switch} from 'react-router-dom';
+import machinelearning from '../assets/images/MachineLearning.jpeg'
+import ML from '../assets/images/ML.jpeg'
+import data from '../assets/images/data.png';
 
-import MetisMenu from 'react-metismenu';
-
-import AboutUs from './components/AboutUs';
-import ContactUs from './components/ContactUs';
-import Dataset from './components/Dataset';
-import Performance from './components/Performance';
-import Visualization from './components/Visualization';
-import Landing from './components/Landing';
-import NotFound from './components/NotFound';
-import FirstPrediction from './components/FirstPrediction';
-import SecondPrediction from './components/SecondPrediction';
-import ThirdPrediction from './components/ThirdPrediction';
+import firststep from '../assets/images/firststep.png';
+import secondstep from '../assets/images/secondstep.png';
+import thirdstep from '../assets/images/thirdstep.png';
 
 
-import logo from './assets/images/whitelogo.png';
 
 
-const menu = [
-    {
-      icon: '',
-      label: <img src={logo} alt="Logo" style={{'border': 'none','height': '100px', 'paddingTop': '20px','paddingBottom': '20px'}}/>,
-      to: '#',
-    },
-    {
-      icon: '',
-      label: 'Home',
-      to: '/',
-    },
-    {
-      icon: '',
-      label: 'Data',
-      content: [
-        {
-          icon: '',
-          label: 'Dataset',
-          to: '/Dataset',
-        },
-        {
-          icon: '',
-          label: 'Visualizaion',
-          to: '/Visualization',
-        },
-      ],
-    },
-    {
-      icon: '',
-      label: 'Prediction',
-      to: '/Performance',
-    },
-    {
-      icon: '',
-      label: 'Social Computing Group',
-      externalLink: true,
-      to: 'https://www.uni-due.de/soco/',
-    },
-  ];
 
 
-function App() {
-  return (
-    
-    <div className="App">
-      <Router>
-      
 
-        <Container fluid>
-              <Row>
-                  <Col md="2">
-                      <MetisMenu
-                      content={menu}
-                      activeLinkFromLocation
-                      />
-                  </Col>
-                  <Col   >
 
-                    <Switch>
-                      <Route exact path="/" component = {Landing} />
-                      <Route exact path="/FirstPrediction" component = {FirstPrediction} />
-                      <Route exact path="/SecondPrediction" component = {SecondPrediction} />
-                      <Route exact path="/ThirdPrediction" component = {ThirdPrediction} />
-                      <Route exact path="/Dataset" component = {Dataset} />
-                      <Route exact path="/Visualization" component = {Visualization} />
-                      <Route exact path="/Performance" component = {Performance} />
-                      <Route exact path="/AboutUs" component = {AboutUs} />
-                      <Route exact path="/ContactUs" component = {ContactUs} />
-                      <Route   component= {NotFound} />
-                    </Switch>
+export default class Performance extends Component {
+    render() {
+        return (
+                <div className="container">
+                    <Row className="justify-content-sm-start mt-3 ">
+                <Header/>
 
-                  </Col>
+                    <div className="card  text-white">
+                      <img src={machinelearning} className="img-fluid" alt="Responsive image" style={{"height":"450px","width":"100%"}}></img>
+                      <div className="card-img-overlay">
+                          <h1 className="card-title text-center font-weight-bold" style={{"font-size":"49px"}} >Machine Learning</h1>
+                          
+                      </div>
+                    </div> 
+                        
+                    <div className="container mt-5">
+                    <p align="justify">Online education has become very popular and plays a positive impact on learning. Organizations have to consider many factors to make the learning process effective. If these are identified correctly, better results can be obtained. Virtual Learning Environment (VLE) provided by Open University has several factors which effect the student's performance. The purpose of this project is to identify the features that affect the student performance. If these are identified correctly, better results can be obtained. Both students and faculties can be well informed about the progress based on the analysis which provides an opportunity to excel.</p>
+
+                    </div>
+                        
+                    <div className="col-sm-6">
+                      <div className="card">
+                        <div className="card-body">
+                          <h5 className="card-title font-weight-bold" style={{"color":"#34666F"}}>Data Preprocessing</h5>
+                          <p className="card-text justify">
+                        The feature engineering part of our analysis consists of merging and feature selection. We used the dataset to train different machine learning algorithms from Scikit-learn. Comparing the different models, we have identified the best suitable model.
+                          It is important to merge all the table and pre-process it. All the missing values are removed. After the data pre-processing, the tables are merged to form a main table which is used for our predictive modelling. </p>
+                        
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="col-sm-6">
+                      <div className="card">
+                        <div className="card-body">
+                          <h5 className="card-title font-weight-bold" style={{"color":"#34666F"}}>Predictive Models</h5>
+                          <p className="card-text justify">
+                          Analysis has been performed on the merged table and different models are implemented to compare and identify the best suited for the dataset. The data is splitted in 80% training and 20% testing. The prediction is done among the four classes - Distinction, Pass, Fail and Withdrawn.                              </p>
+                        </div>
+                      </div>         
+                    </div>                                   
               </Row>
-          </Container>
 
-
-      </Router>
-
-    </div>
-
-
-  );
+            <Row className="justify-content-sm-center mt-3 ">
+              <Col md="4" >
+                      <Link to="/FirstPrediction">
+                          <Card body inverse id="DatasetBox" style={{ height:'200px',textAlign:'center',backgroundColor:'white', paddingTop: '40px' }}>
+                              <CardText><img src={firststep} alt="Performance" style={{'border': 'none','height': '90px'}}></img></CardText>
+                              <CardTitle tag="h6" style={{"color":"#ffba39"}}>I am at the <b>begining</b> of the semester.</CardTitle>
+                          </Card>
+                      </Link>
+                  </Col> 
+                  <Col md="4">
+                      <Link to="/SecondPrediction">
+                          <Card body inverse  id="VisualizationBox" style={{ height:'200px',backgroundColor:'white' ,textAlign:'center', paddingTop: '40px'}}>
+                              <CardText><img src={secondstep} alt="Performance" style={{'border': 'none','height': '90px'}}></img></CardText>
+                              <CardTitle tag="h6" style={{"color":"#0d6bd6"}}>I have done <b>one</b> exam.</CardTitle>
+                          </Card>
+                      </Link>
+                  </Col> 
+                  <Col md="4">
+                      <Link to="/ThirdPrediction">
+                          <Card body inverse  id="PerformanceBox" style={{ height:'200px',backgroundColor:'white', textAlign:'center', paddingTop: '40px'}}>
+                              <CardText><img src={thirdstep} alt="Performance" style={{'border': 'none','height': '90px'}}></img></CardText>
+                              <CardTitle tag="h6" style={{"color":"#24a809"}}>I have done <b>two</b> exams</CardTitle>
+                          </Card>
+                      </Link>
+                  </Col> 
+              </Row>
+              <br></br>
+              <Footer />
+          </div>
+        )
+    }
 }
-
-export default App;
