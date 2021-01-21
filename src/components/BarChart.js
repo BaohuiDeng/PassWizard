e416c304348b65cadc4ad496acdeef07d5ad45c7
AmrShakhshirUDE <67582413+AmrShakhshirUDE@users.noreply.github.com>
2021-01-21 16:10:55 +0100

    Update BarChart.js

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,19 +1,99 @@
+import React, { Component } from 'react';
+import {Bar} from 'react-chartjs-2';
+import axios from 'axios';
 
-import React, { useState, useEffect } from "react";
-import { Bar } from "react-chartjs-2";
-import Hammer from "hammerjs";
-import * as Zoom from "chartjs-plugin-zoom";
 
-const BarChart = () => {
-  const [chartData, setChartData] = useState({});
 
-  const chart = () => {
-    setChartData({
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+
+
+export default  class BarChart extends Component {
+
+  constructor() {
+    super()
+    this.state = 
+      {
+        allInfo:'',
+        Sex:'',
+        GG1:'',
+        traveltime:'',
+        studytime:'',
+        freetime:'',
+        absences:'',
+        Barchartdata:''
+
+
+      }
+
+     
+
+    }
+
+    componentDidMount() {
+
+      axios.get(`http://20.82.112.97:5000/alldata`)
+      .then(res => {
+        const data = res.data;
+        this.setState({allInfo:data})
+        
+        // console.log(this.state.allInfo["0"].school)
+
+        // Create a new array based on current state:
+        let sex = [];
+        let FirstGrade = [];
+        let travelt = [];
+        let studyt = [];
+        let freet = [];
+        let absencesAmount = [];
+
+        for (var i = 0; i < this.state.allInfo.length; i++) {
+              // console.log(this.state.allInfo[i].GG1)
+              // retrieving sex
+              sex.push(this.state.allInfo[i].sex );
+              // retriving first Grade
+              FirstGrade.push(this.state.allInfo[i].GG1 );             
+              travelt.push(this.state.allInfo[i].traveltime );
+              studyt.push(this.state.allInfo[i].studytime );
+              freet.push(this.state.allInfo[i].freetime );
+              absencesAmount.push(this.state.allInfo[i].absences );
+  
+            }
+
+            // make average
+            const average = arr => arr.reduce( ( p, c ) => p + c, 0 ) / arr.length;
+    
+            travelt = average( travelt );
+            studyt = average( studyt );
+            freet = average( freet );
+            absencesAmount = average( absencesAmount );
+            const BarchartdataArray=[];
+            BarchartdataArray.push( travelt , studyt ,freet,absencesAmount  );
+            // Set state
+          this.setState({ Sex:sex , GG1:FirstGrade , traveltime:travelt, studytime: studyt,freetime:freet,absences:absencesAmount,Barchartdata:BarchartdataArray});
+
+          // console.log(this.state.Barchartdata)
+
+
+      }).catch(()=>{
+        alert('Error retrieving data!!');
+    })
+    }
+
+
+
+    render() 
+    {
+      return (
+
+       
+        <div> 
+
+         <Bar
+        data={{
+          labels: ['traveltime', 'studytime', 'freetime', 'absences'],
           datasets: [
             {
-              label: '# of votes',
-              data: [12, 19, 3, 5, 2, 3],
+              label: 'some random Attr vs average',
+              data: this.state.Barchartdata,
               backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
@@ -32,173 +112,41 @@ const BarChart = () => {
               ],
               borderWidth: 1,
             },
-            {
-              label: 'Quantity',
-              data: [47, 52, 67, 58, 9, 50],
-              backgroundColor: 'orange',
-              borderColor: 'red',
-            },
+            // {
+            //   label: 'Quantity',
+            //   data: [47, 52, 67, 58, 9, 50],
+            //   backgroundColor: 'orange',
+            //   borderColor: 'red',
+            // },
           ],
-        
-       
-      
-    });
-  };
-
-  useEffect(() => {
-    chart();
-  }, []);
-
-  return (
-    <div className="App">
-      <h1>Dankmemes</h1>
-      <div>
-        <Bar
-          data={chartData}
-          options={{
-            responsive: true,
-            title: { text: "THICCNESS SCALE", display: true },
-            scales: {
-              yAxes: [
-                {
-                  ticks: {
-                    autoSkip: true,
-                    maxTicksLimit: 10,
-                    beginAtZero: true,
-                  },
-                  gridLines: {
-                    display: false,
-                  },
+        }}
+        height={200}
+        width={300}
+        options={{
+        //   maintainAspectRatio: false,
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true,
                 },
-              ],
-              xAxes: [
-                {
-                  gridLines: {
-                    display: false,
-                  },
-                },
-              ],
+              },
+            ],
+          },
+          legend: {
+            labels: {
+              fontSize: 15,
             },
-            // pan: {
-            //   enabled: true,
-            //   mode: "xy",
-            //   speed: 1,
-            //   threshold: 1,
-            // },
-            // zoom: {
-            //   enabled: true,
-            //   drag: false,
-            //   mode: "xy",
-            //   limits: {
-            //     max: 1,
-            //     min: 0.5,
-            //   },
-            //   rangeMin: {
-            //     x: 2,
-            //     y: 1,
-            //   },
-            //   rangeMax: {
-            //     x: 10,
-            //     y: 150,
-            //   },
-            // },
-            plugins: {
-              zoom: {
-                  // Container for pan options
-                  pan: {
-                      // Boolean to enable panning
-                      enabled: true,
-           
-                      // Panning directions. Remove the appropriate direction to disable
-                      // Eg. 'y' would only allow panning in the y direction
-                      // A function that is called as the user is panning and returns the
-                      // available directions can also be used:
-                      //   mode: function({ chart }) {
-                      //     return 'xy';
-                      //   },
-                      mode: 'xy',
-           
-                      rangeMin: {
-                          // Format of min pan range depends on scale type
-                          x: null,
-                          y: null
-                      },
-                      rangeMax: {
-                          // Format of max pan range depends on scale type
-                          x: null,
-                          y: null
-                      },
-           
-                      // On category scale, factor of pan velocity
-                      speed: 20,
-           
-                      // Minimal pan distance required before actually applying pan
-                      threshold: 10,
-           
-                      // Function called while the user is panning
-                      onPan: function({chart}) { console.log(`I'm panning!!!`); },
-                      // Function called once panning is completed
-                      onPanComplete: function({chart}) { console.log(`I was panned!!!`); }
-                  },
-           
-                  // Container for zoom options
-                  zoom: {
-                      // Boolean to enable zooming
-                      enabled: true,
-           
-                      // Enable drag-to-zoom behavior
-                      drag: true,
-           
-                      // Drag-to-zoom effect can be customized
-                      // drag: {
-                      // 	 borderColor: 'rgba(225,225,225,0.3)'
-                      // 	 borderWidth: 5,
-                      // 	 backgroundColor: 'rgb(225,225,225)',
-                      // 	 animationDuration: 0
-                      // },
-           
-                      // Zooming directions. Remove the appropriate direction to disable
-                      // Eg. 'y' would only allow zooming in the y direction
-                      // A function that is called as the user is zooming and returns the
-                      // available directions can also be used:
-                      //   mode: function({ chart }) {
-                      //     return 'xy';
-                      //   },
-                      mode: 'xy',
-           
-                      rangeMin: {
-                          // Format of min zoom range depends on scale type
-                          x: null,
-                          y: null
-                      },
-                      rangeMax: {
-                          // Format of max zoom range depends on scale type
-                          x: null,
-                          y: null
-                      },
-           
-                      // Speed of zoom via mouse wheel
-                      // (percentage of zoom on a wheel event)
-                      speed: 0.1,
-           
-                      // Minimal zoom distance required before actually applying zoom
-                      threshold: 2,
-           
-                      // On category scale, minimal zoom level before actually applying zoom
-                      sensitivity: 3,
-           
-                      // Function called while the user is zooming
-                      onZoom: function({chart}) { console.log(`I'm zooming!!!`); },
-                      // Function called once zooming is completed
-                      onZoomComplete: function({chart}) { console.log(`I was zoomed!!!`); }
-                  }
-              }
-          }
-          }}
-        />
-      </div>
+          },
+        }}
+      />
     </div>
-  );
-};
 
-export default BarChart;
+
+
+
+        
+      )
+  }
+}
+
