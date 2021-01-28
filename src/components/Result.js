c85f867dc95b58ab48a9efd1e6b4cc13d60c4950
AmrShakhshirUDE <67582413+AmrShakhshirUDE@users.noreply.github.com>
2021-01-28 16:07:22 +0100

    Update Result.js

--- src/components/Result.js
+++ src/components/Result.js
@@ -38,19 +38,88 @@ export default class Result extends Component {
                         <Stepper3/>            </div>
 
                         <Row className="mt-3 ">
-                            <Col md="12 my-4 text-center" style={{"color":"green"}}>
-                                <h3>Congrats! your result is:</h3>
+                            <Col md="12 my-4 text-center" >
 
+                              {
+                                this.props.location.result.msg2 && this.props.location.result.msg2 != 'fail'  &&
+                                <h4 style={{"color":"#55c242"}}>Congrats! You will success.</h4>
+                              }
+                              
+                              {
+                               this.props.location.result.msg44 && this.props.location.result.msg44!= 'fail'  &&
+                                <h4 style={{"color":"#55c242"}}>Congrats! You will success.</h4>
+                              }
+                              {
+                                this.props.location.result.msg66 && this.props.location.result.msg66 != 'fail' &&
+                                <h4 style={{"color":"#55c242"}}>Congrats! You will success.</h4>
+                              }
+                              {
+                                this.props.location.result.msg2 && this.props.location.result.msg2 == 'fail' &&
+                                <h4 style={{"color":"#ff6542"}}>Unfortunately! you will not pass.</h4>
+                              }
+                                                            {
+                               this.props.location.result.msg44 && this.props.location.result.msg44 == 'fail'  &&
+                                <h4 style={{"color":"#ff6542"}}>Unfortunately! you will not pass.</h4>
+                              }
+                              {
+                               this.props.location.result.msg66 && this.props.location.result.msg66 == 'fail' &&
+                                <h4 style={{"color":"#ff6542"}}>Unfortunately! you will not pass.</h4>
+                              }
+                                
                             </Col>
                             <Col md="12" >
                             <Card className="my-3 pt-4"body inverse  style={{ height:'200px',backgroundColor:'white', paddingTop: '5px' }}>
                               <CardText></CardText>
                               <CardTitle tag="h6" className='text-center' style={{"color":"darkblue", fontWeight: "bold"}}>
-                              <img src={resulticon} className={ 'mr-2'} style={{"width":"90px"}}></img>
-                              {this.props.location.result.msg}
+                                <Col md="4" >
+                                  <img src={resulticon} className={ 'mr-2'} style={{"width":"90px" , marginLeft:"30px"}}></img>
+                                </Col>
+                                <Col md="12" style={{ marginTop:"-80px"}} >
+                                {
+                                  this.props.location.result.msg1  &&
+                                  <p>
+                                    The prediction of your First exam with the accuracy of &nbsp; 
+                                    {this.props.location.result.msg1}
+                                    &nbsp;is&nbsp;  
+                                    {this.props.location.result.msg2}
+                                    <br></br>
+                                    <br></br>
+                                    The prediction of your Srcond exam with the accuracy of &nbsp; 
+                                    {this.props.location.result.msg3}
+                                    &nbsp;is&nbsp;  
+                                    {this.props.location.result.msg4}
+                                    <br></br>
+                                    <br></br>
+                                    The prediction of your Third exam with the accuracy of &nbsp; 
+                                    {this.props.location.result.msg5}
+                                    &nbsp;is&nbsp;  
+                                    {this.props.location.result.msg6}
+                                  </p>
+                                }
+                                  
+                                  {
+                                  this.props.location.result.msg33  &&
+                                  <p>
+                                    The prediction of your Second exam with the accuracy of &nbsp;  
+                                    {this.props.location.result.msg33}
+                                    &nbsp; is &nbsp;  
+                                    {this.props.location.result.msg44}
+                                  </p>                          
+                                  }
 
+                                  {
+                                  this.props.location.result.msg55  &&
+                                  <p>
+                                    The prediction of your Third exam with the accuracy of &nbsp;   
+                                    {this.props.location.result.msg55}
+                                    &nbsp;is&nbsp;  
+                                    {this.props.location.result.msg66}
+                                  </p>                          
+                                  }                                
+                                </Col>
+                              
                             </CardTitle>
-                            {/* {this.props.location.result.msg} */}
+                            {/* {this.props.location.result.msg1} */}
                           </Card>
                   </Col> 
                  
