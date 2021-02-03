29cd1e9a941b2ae650ab60a5fac824855f3e9d7f
AmrShakhshirUDE <67582413+AmrShakhshirUDE@users.noreply.github.com>
2021-02-03 13:13:11 +0100

    Update ThirdPrediction.js

--- src/components/ThirdPrediction.js
+++ src/components/ThirdPrediction.js
@@ -210,12 +210,11 @@ export default class SecondPrediction extends Component {
             <div className="container mt-5 pt-4" style={{ borderBottom :"solid 3px #19738A ","height":"70px"}}>
             <h5 className=" text-center" style={{"top":"40%", "color": "#19738A "}}  >Now try to predict your result</h5>
             </div>
-            <h3 className="mt-5 text-center">Fill in the form to get your result!</h3>
 
             <Row className="justify-content-sm-center mt-3 ">
             <Col md="6" >
 
-            <div className="card  border-info mt-5  shadow-lg p-3 mb-5  rounded "> 
+            <div className="card  border-info mt-5   p-3 mb-5 "> 
                  <form>
 
   <div className="form-row">
